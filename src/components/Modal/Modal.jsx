05082ac649009bf61349bff83b349f25688eb91f
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2024-02-16 10:13:29 +0100

    Modal is working, minimal styling

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,12 @@
-export const Modal = () => {
-    return(
-        <div></div>
-    )
-}
+export const Modal = ({ setIsOpen, content }) => {
+  return (
+    <>
+      <div onClick={() => setIsOpen(false)}>
+        <div>
+          <h5>{content}</h5>
+        </div>
+        <button onClick={() => setIsOpen(false)}>x</button>
+      </div>
+    </>
+  );
+};
--- src/pages/frontpage/frontpage.jsx
+++ src/pages/frontpage/frontpage.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { Modal } from "../../components/Modal/Modal";
+
 export const Frontpage = () => {
-    return(
-        <h1>frontpage</h1>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <h1>Frontpage</h1>
+      <section>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        {isOpen && (
+          <Modal
+            setIsOpen={setIsOpen}
+            content="You spin me right 'round, baby, right 'round
+            Like a record, baby, right 'round, 'round, 'round
+            You spin me right 'round, baby, right 'round
+            Like a record, baby, right 'round, 'round, 'round"
+          />
+        )}
+      </section>
+    </>
+  );
+};
