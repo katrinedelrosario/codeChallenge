80322600fd06771fe8c559d669ededca6d0dec48
katrinedelrosario <katrinedelrosario@gmail.com>
2024-02-16 12:31:05 +0100

    slider

--- init.sh
+++ init.sh
@@ -48,15 +48,15 @@ rm -rf react.svg
 cd ../..
 
 echo "<!doctype html>
-<html lang="en">
+<html lang='en'>
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <title></title>
   </head>
   <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
+    <div id='root'></div>
+    <script type='module' src='/src/main.jsx'></script>
   </body>
 </html>" > index.html
 
