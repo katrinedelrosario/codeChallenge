7884ac0064640d1247b21bf37766a92a075857de
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-16 12:33:05 +0100

    404

--- src/pages/404Page/404Page.jsx
+++ src/pages/404Page/404Page.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
 import Styles from "./404Page.module.scss"
 export const PageNotFound = () => {
     return (
         <div className={Styles.container}>
-            <h1><span className={Styles.highlight}>404 page</span> not found</h1>
+            <Link to={"/"}>
+                <h1><span className={Styles.highlight}>404 page</span> not found</h1>
+            </Link>
         </div>
     )
 }
